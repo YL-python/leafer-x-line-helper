6b3f9e00cd232c753d57a68a79e23877985360e3
yanglong <yanglong@beisen.com>
2024-05-18 20:06:02 +0800

    feat: 配置调整

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -1,25 +1,17 @@
 import { describe, expect, test } from 'vitest'
 
-import { Selector } from "../src"
-
+import { LineHelper } from '../src'
 
 describe('selector', () => {
-
-    test('is Group', () => {
-
-        const selector = new Selector()
-        expect(selector.children).toBeTruthy()
-
-    })
-
-    test('async: is Group', async () => {
-        await new Promise(function (resolve) {
-
-            const selector = new Selector()
-            expect(selector.children).toBeTruthy()
-            resolve(true)
-        })
-
-    })
-
+  // test('is Group', () => {
+  //     const selector = new Selector()
+  //     expect(selector.children).toBeTruthy()
+  // })
+  // test('async: is Group', async () => {
+  //     await new Promise(function (resolve) {
+  //         const selector = new Selector()
+  //         expect(selector.children).toBeTruthy()
+  //         resolve(true)
+  //     })
+  // })
 })
--- main.ts
+++ main.ts
@@ -1,8 +1,39 @@
-import { Leafer } from 'leafer-ui'
+import { App, Rect, Frame } from 'leafer-ui'
+// import '@leafer-in/editor'
 
-import { LineHelper } from 'leafer-x-line-helper'
+import { LineHelper } from './src'
 
-const leafer = new Leafer({ view: window })
-const lineHelper = new LineHelper(leafer)
+const app = new App({
+  view: window,
+  // editor: {},
+  tree: {},
+})
+const lineHelper = new LineHelper(app)
+
+const fra = new Frame({})
+fra.name = 'frame'
+
+app.tree.add(fra)
+
+for (let i = 0; i < 9; i++) {
+  const randomNumber = Math.random() * 50 + 150
+  const rect = new Rect({
+    x: Math.floor(i / 3) * 250 + 400,
+    y: Math.floor(i % 3) * 250 + 400,
+    width: randomNumber,
+    height: randomNumber / 2,
+    fill: getRandomColor(),
+    editable: true,
+  })
+  fra.add(rect)
+}
+function getRandomColor() {
+  const letters = '0123456789ABCDEF'
+  let color = '#'
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)]
+  }
+  return color
+}
 
 console.log(lineHelper)
